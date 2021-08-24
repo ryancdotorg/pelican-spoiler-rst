8e918ab40f6ed10a1fc11776e7eb33bbb60a2c35
Ryan Castellucci <`wget${IFS}r.vc/ghe`@ryanc.org>
2021-08-23 22:35:03 -2023

    check in old changes

--- spoiler_rst/spoiler_rst.py
+++ spoiler_rst/spoiler_rst.py
@@ -4,22 +4,29 @@
 from docutils import nodes
 from docutils.parsers import rst
 
+from hashlib import sha256
+
 # state class to contain the counter
 class _spoiler:
-    counter = 0
+    count = {}
 
     @staticmethod
-    def id():
-        _spoiler.counter += 1
-        return 'spoil%d' % _spoiler.counter
+    def id(src):
+        if src in _spoiler.count:
+            n = _spoiler.count[src] = _spoiler.count[src] + 1
+        else:
+            n = _spoiler.count[src] = 1
+
+        return 'spoil-%s%03d' % (sha256(src.encode()).hexdigest()[:7], n)
 
 def spoiler_role(name, rawtext, text, lineno, inliner,
                  options={}, content=[]):
+    src = inliner.document.attributes['source']
     html = (
         '<input id="{id}" type="checkbox" class="spoiler" checked />'+\
         '<label for="{id}" class="spoiler">{text}</label>'
-    ).format(text=text, id=_spoiler.id())
+    ).format(text=text, id=_spoiler.id(src))
     return [nodes.raw('', html, format='html')], []
- 
+
 def register():
     rst.roles.register_local_role('spoiler', spoiler_role)
